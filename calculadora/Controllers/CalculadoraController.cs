8320feb05cf7b91c7f947786ac850b851b511f8e
Ederson Marques <edmakes@gmail.com>
2024-02-20 23:47:59 -0300

    initial commit

---
+++ calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using calculadora.Models;
+
+public class CalculadoraController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new CalculadoraModel());
+    }
+
+    [HttpPost]
+    public IActionResult Index(CalculadoraModel model, string operation)
+    {
+        switch (operation)
+        {
+            case "Somar":
+                model.Resultado = model.Valor1 + model.Valor2;
+                break;
+            case "Subtrair":
+                model.Resultado = model.Valor1 - model.Valor2;
+                break;
+            case "Multiplicar":
+                model.Resultado = model.Valor1 * model.Valor2;
+                break;
+            case "Dividir":
+                if (model.Valor2 != 0)
+                {
+                    model.Resultado = model.Valor1 / model.Valor2;
+                }
+                else
+                {
+                    ModelState.AddModelError("Valor2", "Não é possível dividir por zero");
+                    return View(model);
+                }
+                break;
+        }
+        return View(model);
+    }
+}
---
+++ calculadora/Models/CalculadoraModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculadora.Models
+{
+	public class CalculadoraModel
+	{
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
+
